def807f9c7984f22d467ad9d17dbab0b0db757f8
Dave <dkha055@aucklanduni.ac.nz>
2024-08-26 23:56:26 +1200

    Make homepage better and starting creating project page

--- src/InitialPage.jsx
+++ src/InitialPage.jsx
@@ -4,51 +4,49 @@ import GlitchyText from "./Helper Components/GlitchyText";
 export default function InitialPage() { 
 
     const welcomeCodeStyling = { 
-        fontSize: "1.7rem",
+        fontSize: "1rem",
         color: "#F2ECD5",
         fontFamily: "Big Shoulders Display",
         fontWeight: "700",
         letterSpacing: "0.3rem" 
     }
 
+    const firstNameStyling = { 
+        fontSize: "8rem",
+        color: "#F2ECD5",
+        fontFamily: "Big Shoulders Display",
+        fontWeight: "900"
+    }
+
+    const lastNameStyling = {
+        fontSize: "8rem",
+        color: "#F2ECD5",
+        fontFamily: "Big Shoulders Display",
+        fontWeight: "900"
+    }
+
     return (
         <div className="Main-Container">
+
+            <div className="filler">
+                HELLO 
+            </div>
             <div className="frontPage">
-                <img className="frontPage-image" src="./FrontPageWithCorners.png" />
+                <div className="frontPage-fullname">
+                    <GlitchyText styling={firstNameStyling}text="DIVYANSHU" />
+                    <GlitchyText styling={lastNameStyling} text="DAVE KHADKA" />
+                </div>
+                <div className="WelcomeCode">
+                    <GlitchyText styling={welcomeCodeStyling} text=" //Welcome" />
+                    <GlitchyText styling={welcomeCodeStyling} text= "IF READYTOCONTINUE {" />
+                    <GlitchyText styling={welcomeCodeStyling} text= "&nbsp;&nbsp;&nbsp; SCROLLDOWN()" />
+                    <GlitchyText styling={welcomeCodeStyling} text= "} else {" />
+                    <GlitchyText styling={welcomeCodeStyling} text= "&nbsp;&nbsp;&nbsp; KEEPLOOKING()" />
+                    <GlitchyText styling={welcomeCodeStyling} text= "}" />
+                </div>
             </div>
-
-            <GlitchyText styling={
-                {
-                    position: "absolute",
-                    fontSize: "10rem",
-                    color: "#F2ECD5",
-                    top: "35vh",
-                    left: "10rem",
-                    fontFamily: "Big Shoulders Display",
-                    fontWeight: "900"
-                }
-            } text="DIVYANSHU" />
-
-            <GlitchyText styling={
-                {
-                    position: "absolute",
-                    fontSize: "10rem",
-                    color: "#F2ECD5",
-                    top: "50vh",
-                    left: "10rem",
-                    fontFamily: "Big Shoulders Display",
-                    fontWeight: "900"
-                }
-            } text="DAVE KHADKA" />
-
-            <div className="WelcomeCode">
-                <GlitchyText styling={welcomeCodeStyling} text=" //Welcome" />
-                <GlitchyText styling={welcomeCodeStyling} text= "IF READYTOCONTINUE {" />
-                <GlitchyText styling={welcomeCodeStyling} text= "&nbsp;&nbsp;&nbsp; SCROLLDOWN()" />
-                <GlitchyText styling={welcomeCodeStyling} text= "} else {" />
-                <GlitchyText styling={welcomeCodeStyling} text= "&nbsp;&nbsp;&nbsp; KEEPLOOKING()" />
-                <GlitchyText styling={welcomeCodeStyling} text= "}" />
-            </div>
+            
+           
 
         </div>
     )
